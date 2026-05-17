17a816b4a629873e38b1df357f71645c77d5ed50
Lin.windows <linweijun0528@gmail.com>
2026-05-17 20:38:27 +0800

    feat: add backend

---
+++ backend/src/main/java/com/paperart/backend/TestController.java
@@ -0,0 +1,13 @@
+package com.paperart.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api/test")
+    public String test() {
+        return "Hello Backend";
+    }
+}
---
+++ backend/src/test/java/com/paperart/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paperart.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
